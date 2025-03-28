2aa02c83e3ad33689eeeb3d65eedf7a79896671f
merugaJS <meruga.swaroop229@gmail.com>
2025-03-28 13:20:07 -0400

    changes for CI env

--- e2e/config/wdio.ios.ci.ts
+++ e2e/config/wdio.ios.ci.ts
@@ -4,11 +4,12 @@ export const config = {
     ...mainConfig,
     capabilities: [{
         'appium:platformName': 'iOS',
-        'appium:platformVersion': '17.0',
+        'appium:platformVersion': '17.2',
         'appium:deviceName': 'iPhone 15 Pro',
         'appium:automationName': 'XCUITest',
         'appium:app': `${process.cwd()}/ios/build/Build/Products/Release-iphonesimulator/mobiletestengtakehome.app`,
-        // Enable this if debugging
-        // 'appium:newCommandTimeout': 300
+        'appium:newCommandTimeout': 300,
+        'appium:wdaLaunchTimeout': 240000,
+        'appium:wdaConnectionTimeout': 240000,
     }],
 }
